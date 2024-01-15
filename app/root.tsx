d7ec58bd29fd5b6fe734c8660b3bd3c82eb78760
DarkarBlays <juanpamendoza08@gmail.com>
2024-01-15 18:44:00 -0500

    proyecto terminado

--- app/root.tsx
+++ app/root.tsx
@@ -1,110 +1,144 @@
-import {Meta,Links,Outlet,Scripts,LiveReload,useRouteError, isRouteErrorResponse,Link} from '@remix-run/react'
-import styles from './styles/index.css'
-import Header from './components/header'
-import Footer from './components/footer';
-import { useState } from 'react';
-
+import {
+  Meta,
+  Links,
+  Outlet,
+  Scripts,
+  LiveReload,
+  useRouteError,
+  isRouteErrorResponse,
+  Link,
+} from "@remix-run/react";
+import styles from "./styles/index.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { useState,useEffect } from "react";
 
 export function meta() {
-    return [
-      { charset: "utf-8" },
-      { title: "GuitarLA - Remix" },
-      { name: "viewport", content: "width=device-width,initial-scale=1" },
-    ];
+  return [
+    { charset: "utf-8" },
+    { title: "GuitarLA - Remix" },
+    { name: "viewport", content: "width=device-width,initial-scale=1" },
+  ];
 }
 
-export function links(){
-    return[
-        {
-            rel: 'stylesheet',
-            href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
-        },
-        {
-            rel: 'stylesheet',
-            href: styles,
-        },
-        {
-            rel: 'preconnect',
-            href: 'https://fonts.googleapis.com',
-        },
-        {
-            rel: 'preconnect',
-            href: 'https://fonts.gstatic.com',
-            crossOrigin: "true"
-        },
-        {
-            rel: 'stylesheet',
-            href: 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap',
-        }
-
-    ]
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: "https://necolas.github.io/normalize.css/8.0.1/normalize.css",
+    },
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossOrigin: "true",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap",
+    },
+  ];
 }
 
-export default function App(){
+export default function App() {
 
-    const [carrito,setCarrito] = useState([])
-    const agregarCarrito = guitarra =>{
-        if(carrito.some(guitarraState => guitarraState.id === guitarra.id)){
-            //iterar sobre el arreglo, e identificar el elemento duplicado
-            const carritoActualizado = carrito.map(guitarraState =>{
-                if(guitarraState.id === guitarra.id){
-                    //Reescribir la cantidad
-                    guitarraState.cantidad = guitarra.cantidad
-                }
+  const carritoLS = typeof window !== "undefined" ? JSON.parse(localStorage.getItem("carrito")) ?? []: [];
+  const [carrito, setCarrito] = useState(carritoLS);
 
-                return guitarraState
-            })
-            //añadir al carrito
-            setCarrito(carritoActualizado)
-        }else{
-            //Registro nuevo, agregar al carrito
-            setCarrito([...carrito,guitarra])
-        }
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('carrito', JSON.stringify(carrito))
     }
+  }, [carrito])
 
-    return(
-        <Document>
-            <Outlet
-                context={{
-                    agregarCarrito,
-                    carrito
-                }}
-            />
-        </Document>
-    )
+  const agregarCarrito = (guitarra) => {
+    if (carrito.some((guitarraState) => guitarraState.id === guitarra.id)) {
+      //iterar sobre el arreglo, e identificar el elemento duplicado
+      const carritoActualizado = carrito.map((guitarraState) => {
+        if (guitarraState.id === guitarra.id) {
+          //Reescribir la cantidad
+          guitarraState.cantidad = guitarra.cantidad;
+        }
+
+        return guitarraState;
+      });
+      //añadir al carrito
+      setCarrito(carritoActualizado);
+    } else {
+      //Registro nuevo, agregar al carrito
+      setCarrito([...carrito, guitarra]);
+    }
+  };
+
+  const actualizarCantidad = (guitarra) => {
+    const carritoActualizado = carrito.map((guitarraState) => {
+      if (guitarraState.id === guitarra.id) {
+        guitarraState.cantidad = guitarra.cantidad;
+      }
+      return guitarraState;
+    });
+    setCarrito(carritoActualizado)
+  };
+
+  const eliminarGuitarra = id =>{
+    const carritoActualizado = carrito.filter(guitarraState => guitarraState.id !== id)
+    setCarrito(carritoActualizado)
+  }
+
+  return (
+    <Document>
+      <Outlet
+        context={{
+          agregarCarrito,
+          carrito,
+          actualizarCantidad,
+          eliminarGuitarra
+        }}
+      />
+    </Document>
+  );
 }
 
-function Document({children}){
-    return(
-        <html lang="es">
-            <head>
-                <Meta/>
-                <Links/>
-            </head>
-            <body>
-                <Header/>
-                {children}
-                <Footer/>
-                <Scripts/>
-                <LiveReload/>
-            </body>
-        </html>
-    )
+function Document({ children }) {
+  return (
+    <html lang="es">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
 }
 
 //Manejo de errores
-export function ErrorBoundary(){
-    const error = useRouteError()
+export function ErrorBoundary() {
+  const error = useRouteError();
 
-    if(isRouteErrorResponse(error)){
-        return (
-            <Document>
-                <p className='error'>
-                    {error.status}{error.statusText}
-                </p>
-                <Link className='error-enlace' to="/">Tal vez quieras volver a la pagina principal </Link>
-            </Document>
-        ) 
-    } 
+  if (isRouteErrorResponse(error)) {
+    return (
+      <Document>
+        <p className="error">
+          {error.status}
+          {error.statusText}
+        </p>
+        <Link className="error-enlace" to="/">
+          Tal vez quieras volver a la pagina principal{" "}
+        </Link>
+      </Document>
+    );
+  }
 }
-
--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,7 +1,6 @@
 import { useOutletContext } from '@remix-run/react';
 import styles from '../styles/carrito.css'
-
-
+import { ClientOnly } from 'remix-utils/client-only'
 
 export function links() {
     return [
@@ -21,48 +20,64 @@ export function meta() {
 
 function Carrito() {
 
-    const { carrito } = useOutletContext()
+    const { carrito, actualizarCantidad, eliminarGuitarra } = useOutletContext()
+
+
     return (
-        <main className="contenedor">
-            <h1 className="heading">Carrito de compras</h1>
+        <ClientOnly fallback={"Cargando..."}>
+            {() => 
+                <main className="contenedor">
+                    <h1 className="heading">Carrito de compras</h1>
 
-            <div className="contenido">
-                <div className='carrito'>
-                    <h2>Articulos</h2>
-                    {carrito.length === 0 ? 'carrito vacio' : (
-                        carrito.map(producto => (
-                            <div key={producto.id} className='producto'>
-                                <div>
-                                    <img src={producto.imagen} alt={`imagen del producto ${producto.imagen}`} />
-                                </div>
+                    <div className="contenido">
+                        <div className='carrito'>
+                            <h2>Articulos</h2>
+                            {carrito?.length === 0 ? 'carrito vacio' : (
+                                carrito?.map(producto => (
+                                    <div key={producto.id} className='producto'>
+                                        <div>
+                                            <img src={producto.imagen} alt={`imagen del producto ${producto.imagen}`} />
+                                        </div>
 
-                                <div>
-                                    <p className='nombre'>{producto.nombre}</p>
-                                    <p>Cantidad: </p>
-                                    <select 
-                                        value={producto.cantidad}
-                                        className='select'
-                                    >
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
-                                    </select>
+                                        <div>
+                                            <p className='nombre'>{producto.nombre}</p>
+                                            <p>Cantidad: </p>
+                                            <select
+                                                value={producto.cantidad}
+                                                className='select'
+                                                onChange={e => actualizarCantidad({
+                                                    cantidad: +e.target.value,
+                                                    id: producto.id
+                                                })}
+                                            >
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                            </select>
 
-                                    <p className='precio'>$<span>{producto.precio}</span></p>
-                                    <p className='subtotal'>Subtotal: $ <span>{producto.cantidad * producto.precio}</span></p>
-                                </div>
-                            </div>
-                        ))
-                    )}
-                </div>
-                <aside className="resumen">
-                    <h3>Resumen del Pedido</h3>
-                    <p>Total a pagar: $</p>
-                </aside>
-            </div>
-        </main>
+                                            <p className='precio'>$<span>{producto.precio}</span></p>
+                                            <p className='subtotal'>Subtotal: $ <span>{producto.cantidad * producto.precio}</span></p>
+                                        </div>
+
+                                        <button
+                                            type='button'
+                                            className='btn_eliminar'
+                                            onClick={() => eliminarGuitarra(producto.id)}
+                                        >X</button>
+                                    </div>
+                                ))
+                            )}
+                        </div>
+                        <aside className="resumen">
+                            <h3>Resumen del Pedido</h3>
+                            <p>Total a pagar: <span>${carrito.reduce((total, producto) => total + producto.precio * producto.cantidad, 0)}</span></p>
+                        </aside>
+                    </div>
+                </main>
+            }
+        </ClientOnly>
     )
 }
 
